071d0c6c971908b1299aeed8df570eaf9f7c7fd9
svanshuu <svanshu200331@gmail.com>
2024-07-11 12:02:47 +0530

    first commit

---
+++ html/script.js
@@ -0,0 +1,26 @@
+// function Person()
+// {
+//  console.log("i m vanshi");
+ 
+// }
+// Person();
+// function Person1( name,age){
+//     console.log("name is",name);
+//     console.log("age",age);
+
+// }
+// //Person1( "Anahu",21 );
+// //let data= ["vanshi","akriti","aniika"];
+// //let newdata= [ ...data ," rohit"];
+// //console.log(newdata);
+// let age=21;
+// if(age>=18)
+//     {
+//         console.log("you can paly");
+//     }
+// else{
+//     console.log("you dont");
+// }
+let a=8;
+let b=9;
+console.log(a+b);
